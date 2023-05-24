b154593249d3741631b81e0fb46ffc17f554aa42
insung5189 <insung5189@gmail.com>
2023-05-24 19:11:09 +0900

    인성 엔티티 작업중

--- src/main/java/com/ll/alcohol/product/productEntity/netWeight/NetWeight.java
+++ src/main/java/com/ll/alcohol/product/productEntity/netWeight/NetWeight.java
@@ -3,4 +3,10 @@ import jakarta.persistence.*;
 
 @Entity
 public class NetWeight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String netWeight; // ml단위 지켜서
 }
