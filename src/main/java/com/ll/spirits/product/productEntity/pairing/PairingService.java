966cf2d0d746901469526150ae42a7263a790624
insung5189 <insung5189@gmail.com>
2023-06-12 13:25:18 +0900

    PairingService 추가함

---
+++ src/main/java/com/ll/spirits/product/productEntity/pairing/PairingService.java
@@ -0,0 +1,16 @@
+package com.ll.spirits.product.productEntity.pairing;
+
+import com.ll.spirits.product.productEntity.netWeight.NetWeight;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class PairingService {
+    private final PairingRepository pairingRepository;
+
+    public List<Pairing> getAllPairing() {
+        return pairingRepository.findAll();
+    }
+}
