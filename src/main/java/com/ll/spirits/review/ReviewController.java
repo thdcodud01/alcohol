c9e4d240e24d0cbcc00202db3f1e4cbb4218961b
insung5189 <insung5189@gmail.com>
2023-06-20 23:09:13 +0900

    리뷰, 리뷰좋아요에 앵커기능(작성시 해당위치로 스크롤 이동)적용

--- src/main/java/com/ll/spirits/review/ReviewController.java
+++ src/main/java/com/ll/spirits/review/ReviewController.java
@@ -26,7 +26,11 @@ public class ReviewController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
-    public String createReview(Model model, @PathVariable("id") Integer id, @Valid ReviewForm reviewForm, BindingResult bindingResult, Principal principal) {
+    public String createReview(@PathVariable("id") Integer id,
+                               @Valid ReviewForm reviewForm,
+                               BindingResult bindingResult,
+                               Principal principal,
+                               Model model) {
         Product product = this.productService.getProduct(id);
         // TODO: 리뷰를 저장한다.
         SiteUser siteUser = this.userService.getUser(principal.getName());
@@ -41,13 +45,15 @@ public class ReviewController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String reviewModify(ReviewForm reviewForm, @PathVariable("id") Long id, Principal principal) {
+    public String reviewModify(@PathVariable("id") Long id,
+                               ReviewForm reviewForm,
+                               Principal principal) {
         Review review = this.reviewService.getReview(id);
         if (!review.getAuthor().getUsername().equals(principal.getName())) { // getName Username의 데이터값을 가져옴. getName은 내장함수.
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         reviewForm.setContent(review.getContent());
-        return "review_form";
+        return String.format("redirect:/product/detail/%s#review_%s", review.getProduct().getId(), review.getId());
     }
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
@@ -61,20 +67,18 @@ public class ReviewController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         this.reviewService.modify(review, reviewForm.getContent());
-        return String.format("redirect:/product/detail/%s#review_%s",
-                review.getProduct().getId(), review.getId());
+        return String.format("redirect:/product/detail/%s#review_%s", review.getProduct().getId(), review.getId());
     }
 
 
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/vote/{id}")
-    public String reviewVote(Principal principal, @PathVariable("id") Long id) {
+    public String reviewVote(@PathVariable("id") Long id,
+                             Principal principal) {
         Review review = this.reviewService.getReview(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.reviewService.vote(review, siteUser);
-        return String.format("redirect:/question/detail/%s#answer_%s",
-                review.getProduct().getId(), review.getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", review.getProduct().getId(), review.getId());
     }
-
 }
