4a88e828314e4facc7ff8192b86fb141786ce784
thdcodud01 <thdcodud01@gmail.com>
2023-06-22 16:55:53 +0900

    로그인다시가능하게함(18!!!!!)

--- src/main/java/com/ll/spirits/SecurityConfig.java
+++ src/main/java/com/ll/spirits/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.ll.spirits;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -20,34 +18,29 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    @Autowired
-    private OAuth2UserService oAuth2UserService;
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(
-                        authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers("/**")
-                                .permitAll()
-                )
-                .formLogin(
-                        formLogin -> formLogin
-                                .loginPage("/user/login") // GET
-                                .loginProcessingUrl("/user/login") // POST
-                )
-                .oauth2Login(
-                        oauth2Login -> oauth2Login
-                                .loginPage("/user/login")
-                                .userInfoEndpoint(
-                                        userInfoEndpoint -> userInfoEndpoint
-                                                .userService(oAuth2UserService)
-                                )
-                )
-                .logout(
-                        logout -> logout
-                                .logoutUrl("/user/logout")
-                );
+        http.authorizeHttpRequests()
+                .requestMatchers(
+                        new AntPathRequestMatcher("/**")).permitAll()
+                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                .and()
+                .csrf().ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/**"))
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+        ;
         return http.build();
     }
     @Bean
@@ -58,5 +51,4 @@ public class SecurityConfig {
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
 }
